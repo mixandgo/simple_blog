ca1999420fad5de8271744990009c2bb20d12dd9
Cezar Halmagean <cezar@halmagean.ro>
2013-11-08 22:17:24 +0200

    Post.published scope

--- app/models/simple_blog/post.rb
+++ app/models/simple_blog/post.rb
@@ -1,5 +1,7 @@
 module SimpleBlog
   class Post < ActiveRecord::Base
     validates :title, :presence => true, :length => {:maximum => 72}
+
+    scope :published, -> { where("published_at IS NOT NULL") }
   end
 end
--- spec/models/simple_blog/post_spec.rb
+++ spec/models/simple_blog/post_spec.rb
@@ -4,5 +4,13 @@ module SimpleBlog
   describe Post do
     it { should validate_presence_of(:title) }
     it { should ensure_length_of(:title).is_at_most(72) }
+
+    describe ".published" do
+      it "finds all the published records" do
+        published = FactoryGirl.create(:post)
+        unpublished = FactoryGirl.create(:post, :published_at => nil)
+        Post.published.should == [published]
+      end
+    end
   end
 end

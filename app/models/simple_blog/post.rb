76b67641b32810b08cfd47faa38a39c8509dd544
Cezar Halmagean <cezar@halmagean.ro>
2013-11-08 22:15:27 +0200

    Post#title validation

--- app/models/simple_blog/post.rb
+++ app/models/simple_blog/post.rb
@@ -1,4 +1,5 @@
 module SimpleBlog
   class Post < ActiveRecord::Base
+    validates :title, :presence => true, :length => {:maximum => 72}
   end
 end
--- spec/models/simple_blog/post_spec.rb
+++ spec/models/simple_blog/post_spec.rb
@@ -2,5 +2,7 @@ require 'spec_helper'
 
 module SimpleBlog
   describe Post do
+    it { should validate_presence_of(:title) }
+    it { should ensure_length_of(:title).is_at_most(72) }
   end
 end

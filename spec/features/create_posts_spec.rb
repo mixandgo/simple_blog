1d9f6eee35664cef82be30f9a0cae1618f5376e0
Cezar Halmagean <cezar@halmagean.ro>
2013-11-15 14:34:03 +0200

    Fixed submit translation

--- spec/features/create_posts_spec.rb
+++ spec/features/create_posts_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'Create posts' do
   scenario 'creating a post' do
     create_a_blog_post
-    success_message = '"My new post" has been created'
+    success_message = 'Post was succesfully created.'
     expect(page).to have_content(success_message)
   end
 end

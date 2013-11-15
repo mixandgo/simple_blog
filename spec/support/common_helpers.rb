af6c275d608bf8e902e166a4cc28318c1a1ec9f9
Cezar Halmagean <cezar@halmagean.ro>
2013-11-15 15:07:28 +0200

    Added link to add new posts

--- spec/support/common_helpers.rb
+++ spec/support/common_helpers.rb
@@ -1,5 +1,6 @@
 def create_a_blog_post(options={})
-  visit simple_blog.new_post_path
+  visit simple_blog.root_path
+  click_link 'New post'
   fill_in 'Title', :with => options[:title] || 'My new post'
   fill_in 'Body', :with => options[:body] || 'The post body'
   click_button 'Create post'

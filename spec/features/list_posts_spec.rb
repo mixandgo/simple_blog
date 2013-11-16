cb5e21b014f417a69113391822d119a27a331d04
Cezar Halmagean <cezar@halmagean.ro>
2013-11-16 14:58:39 +0200

    New post link spec

--- spec/features/list_posts_spec.rb
+++ spec/features/list_posts_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'List posts' do
-  scenario 'it lists all the published posts' do
+  scenario 'all the published posts are displayed' do
     create_a_blog_post(:title => 'Cool stuff', :body => 'Cool body')
     create_a_blog_post(:title => 'More cool stuff', :body => 'Cool body')
     create_a_blog_post(:title => 'Unpublished post', :body => 'Cool body', :unpublished => true)
@@ -9,5 +9,11 @@ feature 'List posts' do
     expect(page).to have_content('Cool Stuff')
     expect(page).to have_content('More Cool Stuff')
     expect(page).not_to have_content('Unpublished Post')
+  end
+
+  scenario 'user can create new posts by clicking on a link' do
+    visit simple_blog.posts_path
+    click_on 'New post'
+    expect(page.current_url).to eq(simple_blog.new_post_url)
   end
 end

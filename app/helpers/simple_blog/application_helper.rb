89c95df2ed88f0bd54fde34fddae2899eaa24760
Cezar Halmagean <cezar@halmagean.ro>
2013-11-19 23:27:25 +0200

    Call main_app's helpers

--- app/helpers/simple_blog/application_helper.rb
+++ app/helpers/simple_blog/application_helper.rb
@@ -1,4 +1,11 @@
 module SimpleBlog
   module ApplicationHelper
+    def method_missing(method, *args, &block)
+      if (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && main_app.respond_to?(method)
+        main_app.send(method, *args)
+      else
+        super
+      end
+    end
   end
 end

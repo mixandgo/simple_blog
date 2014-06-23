af79129022b76c40708121bdc291854a1d16a5e1
Marcu Alexandru <amarcu@repairpal.com>
2014-06-23 11:08:40 +0300

    fixed a few features

--- spec/features/list_posts_spec.rb
+++ spec/features/list_posts_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 feature 'List posts' do
+  background do
+    create_a_blog_post(:title => 'Cool stuff', :body => 'Cool body', :tags => 'tag')
+    create_a_blog_post(:title => 'More cool stuff', :body => 'Cool body', :tags => 'tag')
+  end
+
   scenario 'all the published posts are displayed' do
-    create_a_blog_post(:title => 'Cool stuff', :body => 'Cool body')
-    create_a_blog_post(:title => 'More cool stuff', :body => 'Cool body')
     create_a_blog_post(:title => 'Unpublished post', :body => 'Cool body', :unpublished => true)
     visit blog_posts_path
     expect(page).to have_content('Cool Stuff')
@@ -12,19 +15,16 @@ feature 'List posts' do
   end
 
   scenario "posts show tags on index page" do
-    create_a_blog_post(:tags => "first_tag, second_tag")
     visit blog_posts_path
-    expect(page).to have_content("first_tag, second_tag")
+    expect(page).to have_content("tag")
   end
 
   scenario "filtering by tag returns only posts with that tag" do
-    create_a_blog_post(:title => 'First post', :body => 'cool body', :tags => 'tag')
-    create_a_blog_post(:title => 'Second post', :body => 'cool body', :tags => 'tag')
     create_a_blog_post(:title => 'post with different tag', :body => 'cool body', :tags => 'different tag')
     visit blog_posts_path
     click_link("tag", :match => :first)
-    expect(page).to have_content('First Post')
-    expect(page).to have_content('Second Post')
-    expect(page).not_to have_content('post with different tag')
+    expect(page).to have_content('Cool Stuff')
+    expect(page).to have_content('More Cool Stuff')
+    expect(page).not_to have_content('Post With Different Tag')
   end
 end

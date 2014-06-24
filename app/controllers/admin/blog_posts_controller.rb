44adc8e87da8196f206314963c45af0d3249da73
Marcu Alexandru <amarcu@repairpal.com>
2014-06-24 13:59:18 +0300

    refactored admin blog post controller

--- app/controllers/admin/blog_posts_controller.rb
+++ app/controllers/admin/blog_posts_controller.rb
@@ -8,8 +8,7 @@ class Admin::BlogPostsController < Admin::BaseController
   end
 
   def edit
-    permitted = slug_params
-    @blog_post = BlogPost.unscoped.find_blog_post!(permitted.fetch(:id))
+    @blog_post = BlogPost.unscoped.find_blog_post!(slug_params.fetch(:id))
   end
 
   def create
@@ -24,8 +23,7 @@ class Admin::BlogPostsController < Admin::BaseController
   end
 
   def update
-    permitted = slug_params
-    @blog_post = BlogPost.unscoped.find_blog_post!(permitted.fetch(:id))
+    @blog_post = BlogPost.unscoped.find_blog_post!(slug_params.fetch(:id))
     if @blog_post.update(blog_post_params)
       flash[:notice] = t('.flash_notice')
       redirect_to admin_blog_posts_path

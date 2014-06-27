a40a08f965b00936c2ea4f409b51a151b1d5909e
Marcu Alexandru <amarcu@repairpal.com>
2014-06-27 14:40:25 +0300

    removed ckeditor generated file
    removed ckeditor generated file, routes and migration
    updated dummy/db/schema.rb

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140626132540) do
+ActiveRecord::Schema.define(version: 20140622105811) do
 
   create_table "blog_posts", force: true do |t|
     t.string   "title"
@@ -23,22 +23,6 @@ ActiveRecord::Schema.define(version: 20140626132540) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "ckeditor_assets", force: true do |t|
-    t.string   "data_file_name",               null: false
-    t.string   "data_content_type"
-    t.integer  "data_file_size"
-    t.integer  "assetable_id"
-    t.string   "assetable_type",    limit: 30
-    t.string   "type",              limit: 30
-    t.integer  "width"
-    t.integer  "height"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
-  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
 
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"

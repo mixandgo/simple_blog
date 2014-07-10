3c928ae019f85133347cb34e444446bb09f4080c
Marcu Alexandru <amarcu@repairpal.com>
2014-07-10 15:03:55 +0300

    update find_associated_pictures method
    added the id to the method. when id not nil it will return the exact picture

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -7,10 +7,11 @@ class Ckeditor::Picture < Ckeditor::Asset
     url(:content)
   end
 
-  def self.find_associated_pictures(model_name, model_id)
-    self.
+  def self.find_associated_pictures(model_name, model_id, id = nil)
+    pictures = self.
       joins("JOIN blog_imageables ON blog_imageables.ckeditor_assets_id = ckeditor_assets.id").
       where("blog_imageables.imageable_type = ? AND blog_imageables.imageable_id = ?", model_name, model_id)
+    pictures.where("ckeditor_assets.id = ?", id) unless id.nil?
   end
 
 end
--- spec/models/ckeditor/picture_spec.rb
+++ spec/models/ckeditor/picture_spec.rb
@@ -6,9 +6,12 @@ describe Ckeditor::Picture do
     let(:join_sql) { "JOIN blog_imageables ON blog_imageables.ckeditor_assets_id = ckeditor_assets.id" }
     let(:model_name) { BlogPost }
     let(:model_id) { 100 }
+    let(:id) { 100 }
 
     before :each do
       allow(Ckeditor::Picture).to receive(:joins).with(join_sql).and_return sql
+      allow(sql).to receive(:where).
+        with("blog_imageables.imageable_type = ? AND blog_imageables.imageable_id = ?", model_name, model_id).and_return sql
     end
 
     it "searches for pictures associated by model name and model id " do
@@ -18,5 +21,11 @@ describe Ckeditor::Picture do
       Ckeditor::Picture.find_associated_pictures(model_name, model_id)
     end
 
+    it "returns the picture with the specified id" do
+      expect(sql).to receive(:where).
+        with("ckeditor_assets.id = ?", id)
+
+      Ckeditor::Picture.find_associated_pictures(model_name, model_id, id)
+    end
   end
 end

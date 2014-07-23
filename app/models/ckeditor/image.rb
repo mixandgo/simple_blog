045bfd733b299248444b8779ac648a088e2a7c9b
Marcu Alexandru <amarcu@repairpal.com>
2014-07-23 13:22:47 +0300

    refactored ckeditor image model
    removed picture and attachement files from ckeditor. created image
    model. updated ckeditor initializer so it know what the new picture
    model is

--- app/models/ckeditor/image.rb
+++ app/models/ckeditor/image.rb
@@ -1,7 +1,9 @@
-class Ckeditor::Picture < Ckeditor::Asset
+class Ckeditor::Image < Ckeditor::Asset
+
   mount_uploader :data, CkeditorPictureUploader, :mount_on => :data_file_name
 
   def url_content
     url(:content)
   end
+
 end

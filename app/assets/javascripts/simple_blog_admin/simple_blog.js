b5d4e011d5fb8a59b4a8e1e70509c41f30dcb141
Marcu Alexandru <amarcu@repairpal.com>
2014-09-02 13:50:15 +0300

    use module pattern for keyword parser
    fix a scenario definition. changed the keyword parser to use the module pattern

--- app/assets/javascripts/simple_blog_admin/simple_blog.js
+++ app/assets/javascripts/simple_blog_admin/simple_blog.js
@@ -4,7 +4,17 @@ $(document).ready(function() {
   setAutocomplete('simple-blog-post-form-tag-list');
   setAutocomplete('simple-blog-post-form-keyword-list');
   setTitleValidation();
+  setKeywordHandler();
 });
+
+function setKeywordHandler() {
+  CKEDITOR.on("instanceReady", function(evt) {
+    CKEDITOR.instances["simple-blog-post-form-body"].document.on("keyup", function(event) {
+      var ckeditor = CKEDITOR.instances["simple-blog-post-form-body"]
+      KeywordParser.showTopKeywords(ckeditor.getData());
+    });
+  });
+}
 
 function addCkeditor(elementId) {
   if ($("#"+elementId).length) {

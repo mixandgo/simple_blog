8010c0d028f5027d13c20f96aad0646c33bb0730
Marcu Alexandru <amarcu@repairpal.com>
2014-09-09 12:43:46 +0300

    updated keyword parser
    made some changes to the keyword parser javascript

--- app/assets/javascripts/simple_blog_admin/popular_keywords.js
+++ app/assets/javascripts/simple_blog_admin/popular_keywords.js
@@ -1,50 +1,63 @@
 var KeywordParser = (function () {
 
-  var keywords = "";
-  var keywordListLength = 20;
-  var topKeywordsObject = {};
-  var topKeywordsList = [];
-  var allKeywords = [];
+  var keywordsObject = {};
   var blackListKeywords = ["the","be","to","of","and","a","in","that","have","I","it","for","not","on","with","he","as","you","do","at","this","but","his","by","from","they","we","say","her","she","or","an","will","my","one","all","would","there","their","what","so","up","out","if","about","who","get","which","go","me","when","make","can","like","time","no","just","him","know","take","people","into","year","your","good","some","could","them","see","other","than","then","now","look","only","come","its","over","think","also","back","after","use","two","how","our","work","first","well","way","even","new","want","because","any","these","give","day","most","us", "theyre", "youre"];
 
   var setTopKeywordsList = function(html) {
-    // strip html tags
-    // ignore everything except letters
-    text = html.replace(/<\/?[^>]+(>|$)/g, "").replace(/[^a-zA-Z ]/g, "");
-    allKeywords = text.match(/\S+/g);
-    topKeywordsList = [];
-    topKeywordsObject = {};
-    for(var keywordIndex in allKeywords) {
+    keywordsObject = {};
+    var allKeywords = getKeywordsList(html);
+    var keywordsList = [];
+    for (var keywordIndex = 0; keywordIndex < allKeywords.length; keywordIndex++) {
       var keyword = allKeywords[keywordIndex];
+      // skip if keyword length greater then 2 to prevent logging small words
+      // and skip if word in blackList Keywords
       if ((keyword.length <= 2) || (blackListKeywords.indexOf(keyword.toLowerCase()) != -1)) { continue; }
-      if (keyword in topKeywordsObject) {
-        topKeywordsObject[keyword] += 1
+      if (keyword in keywordsObject) {
+        keywordsObject[keyword] += 1
       } else {
-        topKeywordsObject[keyword] = 1
+        keywordsObject[keyword] = 1
       }
     }
   };
 
-  var orderKeywords = function() {
-    for (var keyword in topKeywordsObject) {
-      topKeywordsList.push([keyword, topKeywordsObject[keyword]])
+  var getKeywordsList = function(html) {
+    // strip html tags
+    // ignore everything except letters
+    return html.replace(/<\/?[^>]+(>|$)/g, "").replace(/[^a-zA-Z ]/g, "").match(/\S+/g);
+  };
+
+  var sortKeywords = function() {
+    var keywordsList = [];
+    for (var keyword in keywordsObject) {
+      keywordsList.push([keyword, keywordsObject[keyword]])
     }
-    topKeywordsList = topKeywordsList.sort(function(a, b) {return b[1] - a[1]})
+    return keywordsList.sort(function(a, b) {return b[1] - a[1]});
   };
 
   var getTopKeywordsList = function() {
-    orderKeywords();
-    keywords = "";
-    for(var index = 0; index < topKeywordsList.length; index ++) {
-      if (index > keywordListLength) { break; }
-      keyword = topKeywordsList[index]
-      keywords += "</br>kw: " + keyword[0] + " - nr: " + keyword[1];
+    var allowedKeywordsNumber = 20;
+    var keywordsList = sortKeywords();
+    var topKeywordsList = [];
+
+    for(var index = 0; index < allowedKeywordsNumber; index ++) {
+      if (index < keywordsList.length){
+        topKeywordsList.push(keywordsList[index]);
+      }
     }
+    return topKeywordsList;
+  };
+
+  var formatOutput = function(topKeywordsList) {
+    var keywords = "";
+    for (var index = 0; index < topKeywordsList.length; index++) {
+      keywords += "</br>kw: " + topKeywordsList[index][0] + " - nr: " + topKeywordsList[index][1];
+    }
+    return keywords;
   };
 
   var showTopKeywords = function(html) {
     setTopKeywordsList(html);
-    getTopKeywordsList();
+    var keywords = formatOutput(getTopKeywordsList())
     if ($("#simple-blog-form-keywords-view").length == 0) {
       $("#simple-blog-form-keywords").after("<div id='simple-blog-form-keywords-view'>" + keywords +  "</div>");
     } else {

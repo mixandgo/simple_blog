400793fe1b3ff0a67878802b064c157a7d26dfca
Cezar Halmagean <cezar@halmagean.ro>
2014-09-12 16:23:27 +0300

    Load any decorators from the main app if present

--- lib/simple_blog.rb
+++ lib/simple_blog.rb
@@ -1,4 +1,11 @@
 require "simple_blog/engine"
 
 module SimpleBlog
+  class Engine < ::Rails::Engine
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
+  end
 end

b9c0bbbd7051053d08046fe98834f4e240fbb2a8
Marcu Alexandru <amarcu@repairpal.com>
2014-10-02 13:40:47 +0300

    removed tables. updated schema.rb
    added migrations to remove the imageables and ckeditor assets. updated schema.rb

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901095236) do
+ActiveRecord::Schema.define(version: 20141002101508) do
 
   create_table "blog_posts", force: true do |t|
     t.string   "title"
@@ -20,30 +20,6 @@ ActiveRecord::Schema.define(version: 20140901095236) do
     t.string   "keywords"
     t.datetime "published_at"
     t.string   "slug"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "ckeditor_assets", force: true do |t|
-    t.string   "data_file_name",               null: false
-    t.string   "data_content_type"
-    t.integer  "data_file_size"
-    t.integer  "assetable_id"
-    t.string   "assetable_type",    limit: 30
-    t.string   "type",              limit: 30
-    t.integer  "width"
-    t.integer  "height"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
-  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
-
-  create_table "imageables", force: true do |t|
-    t.integer  "imageable_id"
-    t.string   "imageable_type"
-    t.integer  "ckeditor_assets_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

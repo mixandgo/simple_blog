a87c5d697024f40455133b37752d5b182d20c760
Marcu Alexandru <amarcu@repairpal.com>
2014-10-14 11:51:13 +0300

    created image feature
    added the image features. updated the common helper and add_image
    helper. also updated the test logo image

---
+++ spec/features/images_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+feature 'Images' do
+  scenario 'user can add a image to the blog post' do
+    create_a_blog_post(:title => "Blog Post Title", :image => "test.png")
+    visit admin_blog_posts_path
+    click_on "Blog Post Title"
+    expect(page).to have_content("/uploads/blog_image/image/1/test.png")
+  end
+
+  scenario 'user can add a image from an existing blog post' do
+    create_a_blog_post(:title => "Blog Post Title")
+    visit admin_blog_posts_path
+    click_on "Blog Post Title"
+    attach_image('simple-blog-post-form-image', 'test.png')
+    click_on "Update post"
+    click_on "Blog Post Title"
+    expect(page).to have_content("/uploads/blog_image/image/1/test.png")
+  end
+
+  scenario 'user can add multiple images when editing a blog post' do
+    create_a_blog_post(:title => "Blog Post Title", :image => "test.png")
+    visit admin_blog_posts_path
+    click_on "Blog Post Title"
+    attach_image('simple-blog-post-form-image', 'second_test.png')
+    click_on "Update post"
+    click_on "Blog Post Title"
+    expect(page).to have_content("/uploads/blog_image/image/2/second_test.png")
+  end
+
+  scenario 'deleting existing image', :js => true do
+    create_a_blog_post(:title => "Blog Post Title", :image => "test.png")
+    visit admin_blog_posts_path
+    click_on "Blog Post Title"
+    click_on "Delete Image"
+    page.driver.browser.switch_to.alert.accept
+    expect(page).to_not have_content("/uploads/blog_image/image/1/test.png")
+  end
+end

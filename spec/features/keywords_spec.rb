9564c934d16a6d75ba301a44186d28121551658b
Marcu Alexandru <amarcu@repairpal.com>
2014-10-28 13:50:54 +0200

    make custom design for keyword parser
    added a keyword.css. added classes to the keyword parser and changed the
    specs to match

--- spec/features/keywords_spec.rb
+++ spec/features/keywords_spec.rb
@@ -9,33 +9,33 @@ feature 'Keyword Parser' do
   scenario 'user sees the top keywords while writing an article', :js => true do
     fill_in_body_with "coolbody "*6
     click_on "Create post"
-    expect(page).to have_content('kw: coolbody - nr: 6')
+    expect(page).to have_content('Keyword: "coolbody" Count: 6')
   end
 
   scenario 'user does not see whitespaces as top keywords when writing an article', :js => true do
     fill_in_body_with "coolbody                        "*6
     click_on "Create post"
-    expect(page).to have_content('kw: coolbody - nr: 6')
+    expect(page).to have_content('Keyword: "coolbody" Count: 6')
   end
 
   scenario 'keyword parser should ignore case when counting keywords', :js => true do
     fill_in_body_with "coolbody Coolbody cOOlbody cooLBody COOLBODY coolboDY"
     click_on "Create post"
-    expect(page).to have_content('kw: coolbody - nr: 6')
+    expect(page).to have_content('Keyword: "coolbody" Count: 6')
   end
 
   scenario 'keyword parser should ignore words smaller then 2', :js => true do
     fill_in_body_with "co co co"
     click_on "Create post"
-    expect(page).to_not have_content('kw: co - nr: 3')
+    expect(page).to_not have_content('Keyword: "co" Count: 3')
   end
 
   scenario 'keyword parser should ignore usual words', :js => true do
     fill_in_body_with "the what their"
     click_on "Create post"
-    expect(page).to_not have_content('kw: the - nr: 1')
-    expect(page).to_not have_content('kw: what - nr: 1')
-    expect(page).to_not have_content('kw: their - nr: 1')
+    expect(page).to_not have_content('Keyword: "the" Count: 1')
+    expect(page).to_not have_content('Keyword: "what" Count: 1')
+    expect(page).to_not have_content('Keyword: "their" Count: 1')
   end
 
 end

4d5312b63636c9ac794a803c46c49dd1fe105cc2
Marcu Alexandru <amarcu@repairpal.com>
2015-01-20 16:20:13 +0200

    [#63] scope out language into it's own libray
    scoped out language into it's own module and used it in the form to get
    the language_for_select and the language_full_name from the set language_code

--- app/helpers/blog_post_helper.rb
+++ app/helpers/blog_post_helper.rb
@@ -1,7 +1,9 @@
 require 'blog_markdown'
+require 'blog_languages'
 
 module BlogPostHelper
   include BlogMarkdown
+  include BlogLanguages
 
   def blog_post_tags(post)
     raw post.tag_list.map { |tag| link_to(tag, filter_posts_path(tag)) }.join(", ")
@@ -9,10 +11,6 @@ module BlogPostHelper
 
   def strip_tags_for_seo(text)
     strip_tags(text).strip
-  end
-
-  def languages_for_select
-    ["en", "ro", "af", "ar", "az", "bg", "bn", "bs", "ca", "cs", "cy", "da", "de", "de-AT", "de-CH", "el", "en-AU", "en-CA", "en-GB", "en-IE", "en-IN", "en-NZ", "en-US", "en-ZA", "eo", "es", "es-419", "es-AR", "es-CL", "es-CO", "es-CR", "es-EC", "es-MX", "es-PA", "es-PE", "es-US", "es-VE", "et", "eu", "fa", "fi", "fr", "fr-CA", "fr-CH", "gl", "he", "hi", "hi-IN", "hr", "hu", "id", "is", "it", "it-CH", "ja", "km", "kn", "ko", "lo", "lt", "lv", "mk", "mn", "ms", "nb", "ne", "nl", "nn", "or", "pl", "pt", "pt-BR", "rm", "ru", "sk", "sl", "sr", "sv", "sw", "ta", "th", "tl", "tr", "uk", "ur", "uz", "vi", "wo", "zh-CN", "zh-HK", "zh-TW", "zh-YUE"]
   end
 
 end

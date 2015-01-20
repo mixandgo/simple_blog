cfeddb02930cb3d7ae85f44da7e7e2e8a2ffc932
Marcu Alexandru <amarcu@repairpal.com>
2015-01-20 12:49:52 +0200

    [#63] made it easier for us to use romanian
    moved the romanian language next to english so it will be easier for us
    to create romanian posts otherwise you can spend a lot of time looking
    for the language

--- app/helpers/blog_post_helper.rb
+++ app/helpers/blog_post_helper.rb
@@ -12,7 +12,7 @@ module BlogPostHelper
   end
 
   def languages_for_select
-    ["en", "af", "ar", "az", "bg", "bn", "bs", "ca", "cs", "cy", "da", "de", "de-AT", "de-CH", "el", "en-AU", "en-CA", "en-GB", "en-IE", "en-IN", "en-NZ", "en-US", "en-ZA", "eo", "es", "es-419", "es-AR", "es-CL", "es-CO", "es-CR", "es-EC", "es-MX", "es-PA", "es-PE", "es-US", "es-VE", "et", "eu", "fa", "fi", "fr", "fr-CA", "fr-CH", "gl", "he", "hi", "hi-IN", "hr", "hu", "id", "is", "it", "it-CH", "ja", "km", "kn", "ko", "lo", "lt", "lv", "mk", "mn", "ms", "nb", "ne", "nl", "nn", "or", "pl", "pt", "pt-BR", "rm", "ro", "ru", "sk", "sl", "sr", "sv", "sw", "ta", "th", "tl", "tr", "uk", "ur", "uz", "vi", "wo", "zh-CN", "zh-HK", "zh-TW", "zh-YUE"]
+    ["en", "ro", "af", "ar", "az", "bg", "bn", "bs", "ca", "cs", "cy", "da", "de", "de-AT", "de-CH", "el", "en-AU", "en-CA", "en-GB", "en-IE", "en-IN", "en-NZ", "en-US", "en-ZA", "eo", "es", "es-419", "es-AR", "es-CL", "es-CO", "es-CR", "es-EC", "es-MX", "es-PA", "es-PE", "es-US", "es-VE", "et", "eu", "fa", "fi", "fr", "fr-CA", "fr-CH", "gl", "he", "hi", "hi-IN", "hr", "hu", "id", "is", "it", "it-CH", "ja", "km", "kn", "ko", "lo", "lt", "lv", "mk", "mn", "ms", "nb", "ne", "nl", "nn", "or", "pl", "pt", "pt-BR", "rm", "ru", "sk", "sl", "sr", "sv", "sw", "ta", "th", "tl", "tr", "uk", "ur", "uz", "vi", "wo", "zh-CN", "zh-HK", "zh-TW", "zh-YUE"]
   end
 
 end

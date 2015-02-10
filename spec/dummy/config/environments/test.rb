370f09ace62ba54d6735b7c4e7b14c797dfffddc
Cezar Halmagean <cezar@mixandgo.com>
2015-02-10 17:23:57 +0200

    Update dummy app

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -33,5 +33,4 @@ Dummy::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
-  config.active_record.raise_in_transactional_callbacks = true
 end
